3cf51077842b259ca331dcfcec95a150a5776857
ludoff <ludovicofiore25@gmail.com>
2024-06-05 16:07:45 +0200

    aggiunta età, prezzo al km, calcolo sconti e prezzo in toFixed

--- js/main.js
+++ js/main.js
@@ -5,27 +5,57 @@
 // - va applicato uno sconto del 40% per gli over 65.
 
 
-// richiesta km da percorrere
-const askKm = document.querySelector(".inputkm");
+// selezione input km da percorrere
+const askKm = document.querySelector(".input-km");
 
 console.log(askKm);
 
-// richiesta età
+// selezione input età
+const askAge = document.querySelector(".input-age");
+
+console.log(askAge);
 
 
-// calcolo prezzo al km
-
-
-// calcolo eventuale sconto
-
-
-// gestione bottone
+// gestione attività del bottone al click
 
 const myButton = document.querySelector(".input-button");
 
 myButton.addEventListener("click",
     function(){
-        let inputValueKm = askKm.value;
-        console.log(inputValueKm);
+        // value km
+        let inputValueKm = parseInt(askKm.value);
+
+        // value age
+        let inputValueAge = parseInt(askAge.value);
+        
+        console.log(inputValueKm, inputValueAge);
+
+        // calcolo prezzo al km
+        let firstPrice = inputValueKm * 0.21
+
+        console.log(firstPrice);
+
+        // applicazione eventuali sconti
+        let finalPrice;
+
+        if (inputValueAge < 18) { // se minorenne sconto 20%
+
+            finalPrice = firstPrice - (firstPrice * 0.20);
+            console.log(finalPrice);
+        
+        } else if (inputValueAge > 65) { // se over 65 sconto 40%
+        
+            finalPrice = firstPrice - (firstPrice * 0.40);
+            console.log(finalPrice);
+        
+        } else { // altrimenti prezzo normale
+            finalPrice = firstPrice;
+        }
+
+        // aggiustamento prezzo a due decimali
+        let refinedPrice = finalPrice.toFixed(2);
+        console.log(refinedPrice);
     }
 )
+
+
